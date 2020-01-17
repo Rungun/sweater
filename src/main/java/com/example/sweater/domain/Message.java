1d8b63a73b83a4f1a1ad392e6a8520fc26f05d87
vmukhametzyanov <Rungun>
2020-01-17 15:51:35 +0300

    Full lesson code. Add authorisation and sate DB

--- src/main/java/com/example/sweater/domain/Message.java
+++ src/main/java/com/example/sweater/domain/Message.java
@@ -8,7 +8,7 @@ import javax.persistence.Id;
 @Entity
 public class Message {
     @Id
-    @GeneratedValue(strategy= GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Integer id;
 
     private String text;
